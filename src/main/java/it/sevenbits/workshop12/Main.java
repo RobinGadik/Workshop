5fa9af8ab0e62b662a71b13be1cc7d8934f6bead
Konstantin <kkpomsk@gmail.com>
2018-10-12 18:57:23 +0600

    matrix init

--- src/main/java/it/sevenbits/workshop12/Main.java
+++ src/main/java/it/sevenbits/workshop12/Main.java
@@ -1,7 +1,15 @@
 package it.sevenbits.workshop12;
 
+import it.sevenbits.workshop12.matrix.Matrix;
+
 public class Main {
     public static void main(String[] args){
+        Matrix m = new Matrix(4 ,3);
+        System.out.println(m.toString());
+
+        m.inverseMatrix();
+        System.out.println(m.toString());
+
 
     }
 }
