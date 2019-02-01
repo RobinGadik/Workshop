ba970a2d86902d82916ab9e63559fc7b4306697e
Gadik <kkpomsk@gmail.com>
2019-02-01 22:08:09 +0000

    Mesageformatter map

--- src/main/java/ICollection.java
+++
@@ -1,7 +0,0 @@
-
-import java.io.IOException;
-
-public interface ICollection {
-    void add(User user) throws IOException;
-    User get(long index) throws IOException;
-}
---
+++ src/main/java/it/sevenbits/courses/sm/log/MessageFormatter.java
@@ -0,0 +1,26 @@
+package it.sevenbits.courses.sm.log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MessageFormatter {
+
+    Map<String, String> map;
+
+    public MessageFormatter() {
+        map = new HashMap<>();
+        map.put("MESSAGE", "Part of message: %1$s");
+        map.put("TRASH", "Trash received");
+        map.put("MESSAGE_START", "Message creating started");
+        map.put("MESSAGE_FINISH", "Message creating finished");
+    }
+
+    public String getStringFormatByType(String type){
+
+        if (map.keySet().contains(type)) {
+            return map.get(type);
+        } else {
+            return "Unknown package type";
+        }
+    }
+}
